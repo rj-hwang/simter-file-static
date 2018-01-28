58ae42ad066b393fc1b4b9360be5c662a2e403c8
RJ.Hwang <rongjihuang@gmail.com>
2018-01-28 17:17:06 +0800

    Quick Start

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simter File Server For Static Files
+
+## Initial
+
+```
+npm install -g parcel-bundler
+npm install
+```
+
+## Development
+
+```
+parcel src/index.html
+```
+
+## Build
+
+```
+parcel build src/index.html
+```
---
+++ src/index.js
@@ -0,0 +1,14 @@
+// 引入另一个组件
+import main from './main';
+import Vue from 'vue';
+
+main();
+
+vueP.then(function(Vue) {
+  new Vue({
+    el: "#app",
+    data: {
+      name: "Simter File Server For Static Files"
+    }
+  });
+});
